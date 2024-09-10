0793f502e6fc3c9450806706719814cee16b8020
Gustavo dos santos da silva <gustavosiova10@gmail.com>
2024-09-10 16:53:50 -0300

    Nova Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    
     }
+                    byte idade = 123;
+                    short ano = 2021;
+                    int cep = 23078110; // se começar com o zero, talvez tenha que ser outro tipo
+                    long cpf = 19209303709L; // se começar com o zero, talvez tenha que ser outro tipo // precisa terminar
+                    float pi = 3.14F; // tipo float sempre por f ou F no final
+                    double salario = 2500.50;  // tipo double sempre por d ou D no final
 }
+
