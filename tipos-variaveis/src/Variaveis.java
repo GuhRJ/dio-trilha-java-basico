6de538f6b5c1fe78a4a59b7463f3401aafdc9083
Gustavo dos santos da silva <gustavosiova10@gmail.com>
2024-09-10 17:13:52 -0300

    Variaveis

---
+++ tipos-variaveis/src/Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+                    byte idade = 123;
+                    short ano = 2021;
+                    int cep = 23078110; // se começar com o zero, talvez tenha que ser outro tipo
+                    long cpf = 19209303709L; // se começar com o zero, talvez tenha que ser outro tipo // precisa terminar
+                    float pi = 3.14F; // tipo float sempre por f ou F no final
+                    double salario = 2500.50;  // tipo double sempre por d ou D no final
+    }
+}
